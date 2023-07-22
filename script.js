4e5dae8d33e79afe9cdfa4b9e3b1c75fdd4c2250
elahe01~ <elahe.narimani@gmail.com>
2023-07-22 18:48:08 +0330

    craete project

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+"use strict"
+let number=prompt();
+let str="";
+for(let i=1 ; i<=number ; i++){
+        for(let j=0 ; j<i ; j++){
+            str =str+"*";  
+            } 
+console.log(str);
+str="";
+}
